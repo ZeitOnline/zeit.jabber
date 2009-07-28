6feb770effe5f34d12d32318637b9b02d8c87ea0
Christian Zagrodnick <cz@gocept.com>
2009-07-28 11:23:23 +0000

    vb

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="zeit.jabber",
-    version="0.2dev",
+    version="0.3dev",
     description="Jabber",
     author='Martijn Faassen, Christian Zagrodnick',
     author_email='faassen@startifact.com, cz@gocept.com',
