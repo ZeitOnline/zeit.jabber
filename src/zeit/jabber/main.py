e215f6bc4af6e42937c2370485aa86235e4303b6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-30 11:20:43 +0200

    ZON-3306: Simplify spelling

--- src/zeit/jabber/main.py
+++ src/zeit/jabber/main.py
@@ -10,19 +10,15 @@ def main(config_file):
         __name__, 'schema.xml'))
     conf, handler = ZConfig.loadConfigFile(schema, open(config_file))
     conf.eventlog.startup()
+
     cms = xmlrpclib.ServerProxy(conf.cms.url)
     methods = tuple(method.strip() for method in conf.cms.methods.split())
-
-    def jabber_client_factory():
-        return zeit.jabber.jabber.get_jabber_client(
-            conf.jabber.user, conf.jabber.password, conf.jabber.group)
-    main_loop(
-        cms, methods, jabber_client_factory, conf.jabber.ignore)
-
-
-def main_loop(cms, methods, jabber_client_factory, ignore):
     notifier = zeit.jabber.xmlrpc.Notifier(cms, methods)
-    reader = zeit.jabber.jabber.Reader(jabber_client_factory, notifier, ignore)
+
+    reader = zeit.jabber.jabber.Reader(
+        lambda: zeit.jabber.jabber.get_jabber_client(
+            conf.jabber.user, conf.jabber.password, conf.jabber.group),
+        notifier, conf.jabber.ignore)
 
     while True:
         reader.process()
