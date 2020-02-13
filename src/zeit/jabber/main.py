5f2ca18407b2f1bd77e14f7cee65d421d4c2f41a
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-02-13 15:24:08 +0100

    Add threading and move connect()-function in JabberClient

--- src/zeit/jabber/main.py
+++ src/zeit/jabber/main.py
@@ -6,7 +6,6 @@ import os.path
 import threading
 import zeit.jabber.jabber
 import zope.dottedname.resolve
-import sleekxmpp.exceptions
 import logging
 
 log = logging.getLogger(__name__)
@@ -35,20 +34,11 @@ def main(argv=None):
     jabber['queue'] = notifier
     reader = zeit.jabber.jabber.from_config(jabber)
 
-    if reader.connect():
-        log.info("Connecting to jabber server as %s", jabber['user'])
-        reader.process(block=True)
-    else:
-        log.error("Could not connect to webdav server.")
-        raise sleekxmpp.exceptions.XMPPError('')
-
-    log.info('SCRIPT END')
-
-    #threads = []
-    #threads.append(threading.Thread(target=reader.process))
-    #if hasattr(notifier, 'process'):
-    #    threads.append(threading.Thread(target=notifier.process))
-    #for t in threads:
-    #    t.start()
-    #for t in threads:
-    #    t.join()
+    threads = []
+    threads.append(threading.Thread(target=reader.process))
+    if hasattr(notifier, 'process'):
+        threads.append(threading.Thread(target=notifier.process))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
