48b7766a1d64c66027f1bc0cc5d690fcc0546284
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2023-07-26 16:36:59 +0200

    MAINT: Switch to PEP420 namespace packages

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 
 setup(
     name="zeit.jabber",
-    version='1.4.1.dev0',
+    version='1.5.0.dev0',
     description="XMPP client",
     author='Martijn Faassen, gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     license='BSD',
-    packages=find_packages('src'),
+    packages=find_namespace_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
         'slixmpp',
         'zope.dottedname',
     ],
