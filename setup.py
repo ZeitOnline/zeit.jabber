98413a635414fe02d89c5aa134410f0576271230
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2023-07-26 16:53:39 +0200

    Back to development: 1.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="zeit.jabber",
-    version='1.5.0',
+    version='1.5.1.dev0',
     description="XMPP client",
     author='Martijn Faassen, gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
